bd1d4e2e27bd4d1ffe51bdcbd82ba2382dabc640
GabrielMelo01 <borges.gm22@gmail.com>
2023-08-31 11:50:35 -0300

    aula 16 - ngOnChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: `
+  <router-outlet></router-outlet>
+  <app-title title="Olá Mundo!"></app-title>
+  `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(){}
 
   ngOnInit(): void {
-    setTimeout( () => {
-      console.log(1);
-    },5000)
   }
 }
